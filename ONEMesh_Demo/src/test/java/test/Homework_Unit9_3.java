8e16ffce703c36a39eb37ea5e0ace0adb480e295
unknown <khaclinh09@gmail.com>
2023-10-25 11:46:21 +0700

    Add 2510

--- ONEMesh_Demo/src/test/java/test/Homework_Unit9_3.java
+++ ONEMesh_Demo/src/test/java/test/Homework_Unit9_3.java
@@ -14,10 +14,12 @@ public class Homework_Unit9_3 {
 		String autoURL = "https://selectorshub.com/xpath-practice-page/";
 		WebDriver driver = cb.initChromeDriver(autoURL);
 		cb.pause(5000);
-		WebElement element1 = driver.findElement(By.xpath("//table[@id= 'resultTable']//td[@class= 'left']//a[3]"));
+		WebElement element1 = driver.findElement(By.xpath("//table[@id= 'resultTable']//td[@class= 'left']//a[text() = 'John.Smith']"));
 		System.out.println(element1.getText());
-		
-	
+		WebElement element2 = driver.findElement(By.xpath("//table[@id= 'resultTable']//td[@class= 'left']//a[text() = 'Jordan.Mathews']"));
+		System.out.println(element2.getText());
+		WebElement element3 = driver.findElement(By.xpath("//table[@id= 'resultTable']//td[@class= 'left']//a[text() = 'Kevin.Mathews']"));
+		System.out.println(element3.getText());
 		
 		
 		cb.pause(3000);
--- ONEMesh_Demo/src/test/java/test/Homework_Unit9_4.java
+++ ONEMesh_Demo/src/test/java/test/Homework_Unit9_4.java
@@ -14,11 +14,17 @@ public class Homework_Unit9_4 {
 		String autoURL = "https://www.google.com.vn/";
 		WebDriver driver = cb.initChromeDriver(autoURL);
 		cb.pause(5000);
-		WebElement logo = driver.findElement(By.xpath("//div[@class = 'k1zIA rSk4se']"));
+		WebElement logo = driver.findElement(By.xpath("//div[@class = 'k1zIA rSk4se']//img[@class = 'lnXdpd']"));
 		System.out.println(logo.getText());
-		
-		
-		
+		cb.pause(3000);
+		WebElement text1 = driver.findElement(By.xpath("//div[@class = 'a4bIc']//textarea[@class = 'gLFyf']"));
+		text1.sendKeys("phone");
+		cb.pause(3000);
+		WebElement click = driver.findElement(By.xpath("//div[@class = 'L3eUgb']//div[@class = 'o3j99 ikrT4e om7nvf']"));
+		click.click();
+		WebElement btn1 = driver.findElement(By.xpath("//div[@class = 'FPdoLc lJ9FBc']//input[@class = 'gNO89b']"));
+		btn1.click();
+
 		cb.pause(3000);
 		cb.quitDriver(driver);
 	}
